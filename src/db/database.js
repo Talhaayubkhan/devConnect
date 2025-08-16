72ed1ecb2088f849f57f7f016f7fa6a02c359c39
Talha Ayub <103215272+Talhaayubkhan@users.noreply.github.com>
2025-08-16 21:49:43 +0500

    Add dotenv support and configure MongoDB connection using environment variables

--- src/db/database.js
+++ src/db/database.js
@@ -1,8 +1,6 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  await mongoose.connect(
-    "mongodb+srv://talhaayub:hello2002@cluster0.78mtw8c.mongodb.net/DevConnect"
-  );
+  await mongoose.connect(process.env.MONGO_URI);
 };
 module.exports = connectDB;
