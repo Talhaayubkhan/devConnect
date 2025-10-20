24bd264c5d4339ef28eb5306e194effc6f5a4953
Talha Ayub <103215272+Talhaayubkhan@users.noreply.github.com>
2025-10-20 15:34:12 +0500

    fix: update axios baseURL configuration to conditionally use localhost for development

--- devConnect-frontend/src/api/axios.js
+++ devConnect-frontend/src/api/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: import.meta.env.DEV
+    ? "http://localhost:4000"
+    : import.meta.env.VITE_API_BASE_URL,
   withCredentials: true,
 });
 
